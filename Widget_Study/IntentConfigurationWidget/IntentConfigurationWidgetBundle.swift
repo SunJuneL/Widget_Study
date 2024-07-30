474b2049cd438aae268b468c13373369fad2f731
SunJuneL <sunjoon96@gmail.com>
2024-07-30 17:06:23 +0900

    feat: 프로젝트에 위젯킷 추가
    위젯 추가하는 과정에서 IntentConfiguration과 StaticConfiguration이 있는 것을 알게되었고, 이 둘의 차이점과 여기서 말하는 Configuration이 무엇인지 확인해 보기 위해 둘 다 추가함.

---
+++ Widget_Study/IntentConfigurationWidget/IntentConfigurationWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  IntentConfigurationWidgetBundle.swift
+//  IntentConfigurationWidget
+//
+//  Created by SID on 7/30/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct IntentConfigurationWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        IntentConfigurationWidget()
+    }
+}
---
+++ Widget_Study/StaticConfigurationWidget/StaticConfigurationWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StaticConfigurationWidgetBundle.swift
+//  StaticConfigurationWidget
+//
+//  Created by SID on 7/30/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StaticConfigurationWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        StaticConfigurationWidget()
+    }
+}
