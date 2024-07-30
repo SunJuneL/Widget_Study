0913aafe09b33ff6664fe35e315e9c3597e78da8
SunJuneL <sunjoon96@gmail.com>
2024-07-30 15:35:46 +0900

    feat: Widget 공부를 위한 프로젝트 생성

---
+++ Widget_Study/Widget_Study/Widget_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Widget_StudyApp.swift
+//  Widget_Study
+//
+//  Created by SID on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Widget_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
